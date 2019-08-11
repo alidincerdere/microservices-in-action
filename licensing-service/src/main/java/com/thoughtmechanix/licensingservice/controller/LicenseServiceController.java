0d2b712d2f1df6322018ab25fa003d62b8f81820
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2019-08-11 15:19:07 +0300

    first commit

---
+++ licensing-service/src/main/java/com/thoughtmechanix/licensingservice/controller/LicenseServiceController.java
@@ -0,0 +1,21 @@
+package com.thoughtmechanix.licensingservice.controller;
+
+import com.thoughtmechanix.licensingservice.models.License;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by adere on 10.08.2019.
+ */
+@RestController
+@RequestMapping (value = "/v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+
+    @RequestMapping(value ="{licenseId}" , method = RequestMethod.GET)
+    public License getLicences(@PathVariable("organizationId") String organizationId,
+                               @PathVariable("licenseId") String licenseId) {
+        return new License().withId(licenseId);
+    }
+}
